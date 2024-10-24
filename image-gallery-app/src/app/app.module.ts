10e3519f6c4e49730ca6fd44d557d0ade9864233
luca-andrei51003 <luca.culcean@gmail.com>
2024-10-24 19:15:21 +0300

    solved errors

--- image-gallery-app/src/app/app.module.ts
+++ image-gallery-app/src/app/app.module.ts
@@ -2,21 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // If using forms like dropdowns
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 import { ImageGalleryComponent } from './components/image-gallery/image-gallery.component';
+import { bootstrapApplication } from '@angular/platform-browser';
 
 @NgModule({
   declarations: [
-    AppComponent,
     ImageGalleryComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
 })
 export class AppModule { }
--- image-gallery-app/src/app/components/image-gallery/image-gallery.component.ts
+++ image-gallery-app/src/app/components/image-gallery/image-gallery.component.ts
@@ -1,32 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageService } from '../../services/image-fetch.service';
 
+interface ImageData {
+  author: string;
+  download_url: string;
+}
+
 @Component({
   selector: 'app-image-gallery',
   templateUrl: './image-gallery.component.html',
   styleUrls: ['./image-gallery.component.css']
 })
+
 export class ImageGalleryComponent implements OnInit {
-  images: any[] = [];
+  images: ImageData[] = [];
   authors: string[] = [];
   selectedAuthor: string = '';
-  filteredImages: any[] = [];
+  filteredImages: ImageData[] = [];
   currentImageIndex: number = 0;
 
   constructor(private imageService: ImageService) { }
 
   ngOnInit(): void {
-    this.imageService.getImages().subscribe(data => {
+    this.imageService.getImages().subscribe((data: ImageData[]) => {
       this.images = data;
-      this.authors = [...new Set(data.map((image: any) => image.author))]; // Get unique authors
+      this.authors = [...new Set(data.map(image => image.author))];
     });
   }
 
-  onAuthorSelect(author: string): void {
-    this.selectedAuthor = author;
-    this.filteredImages = this.images.filter(image => image.author === author);
-    this.currentImageIndex = 0; // Reset to the first image
-  }
+  onAuthorSelect(event: Event): void {
+    const target = event.target as HTMLSelectElement;
+    this.selectedAuthor = target.value;
+    this.filteredImages = this.images.filter(image => image.author === this.selectedAuthor);
+    this.currentImageIndex = 0;
+  }  
 
   nextImage(): void {
     if (this.currentImageIndex < this.filteredImages.length - 1) {
